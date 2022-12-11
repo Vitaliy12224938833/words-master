5be1cc3a5428039c7e921af471d771b4b7a39b2b
Vitaliy12224938833 <vitalikd247@gmail.com>
2022-12-11 20:26:32 +0300

    fixed double painting single letter v2

--- words-master/scripts/script.js
+++ words-master/scripts/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const letters = document.querySelectorAll(".scoreboard-letter");
 const body = document.querySelector(".body");
 const restartBtn = document.querySelector(".restart-btn");
@@ -38,53 +39,71 @@ const obj = {
       body: JSON.stringify({ word: currWord }),
     });
     const validObj = await promis.json();
-    obj.valid = validObj.validWord;
-    obj.paintingLetters();
+    this.valid = validObj.validWord;
+    this.paintingLetters();
   },
 
   getSekretWord: async function () {
     const promis = await fetch(WORDS_URL);
     const wordObj = await promis.json();
-    obj.secretWord = wordObj.word;
+    this.secretWord = wordObj.word;
+    console.log(this.secretWord);
   },
 
   paintingLetters() {
-    const currSize = obj.currIdx + WORD_SIZE;
-    if (!obj.valid) {
-      for (let i = obj.currIdx; i < currSize; i++) {
+    const { secretWord, numOfLetter, index, valid, currWord, currIdx } = this;
+
+    const currSize = currIdx + WORD_SIZE;
+
+    if (!valid) {
+      for (let i = currIdx; i < currSize; i++) {
         letters[i].style.borderColor = "#FF0000";
         letters[i].style.transition = "0.5s";
       }
       setTimeout(() => {
-        for (let j = obj.currIdx; j < currSize; j++) {
+        for (let j = currIdx; j < currSize; j++) {
           letters[j].style.borderColor = "#333";
         }
       }, 500);
     } else {
       let idx = 0;
-      const chopSecretWord = obj.secretWord.split("");
-      for (let i = obj.currIdx; i < currSize; i++) {
+
+      const arrCurrWord = currWord.split("");
+      const arrSecretWord = secretWord.split("");
+
+      for (let i = currIdx; i < currSize; i++) {
         const letterStyle = letters[i].style;
-        const currLetter = obj.currWord[idx];
-        const secretLetter = obj.secretWord[idx];
-        const chopIndex = chopSecretWord.indexOf(currLetter);
-        letterStyle.transition = "";
-        if (currLetter === secretLetter) {
+        const currWordLetter = currWord[idx];
+        const secretWordLetter = secretWord[idx];
+
+        const numOfLetCurrWord = numOfLetter(arrCurrWord, currWordLetter);
+        const numOfLetSecretWord = numOfLetter(arrSecretWord, currWordLetter);
+
+        const chopIndex = arrCurrWord.indexOf(currWordLetter);
+
+        letterStyle.transition = "0.1s";
+
+        if (currWordLetter === secretWordLetter) {
           letterStyle.backgroundColor = "green";
+          if (numOfLetCurrWord > numOfLetSecretWord) {
+            arrCurrWord.splice(chopIndex, 1);
+          }
         } else if (
-          obj.secretWord.indexOf(currLetter) !== -1 &&
-          chopIndex !== -1
+          !numOfLetSecretWord ||
+          numOfLetCurrWord > numOfLetSecretWord
         ) {
-          letterStyle.backgroundColor = "#ffff00";
-        } else {
+          if (numOfLetCurrWord > numOfLetSecretWord) {
+            arrCurrWord.splice(chopIndex, 1);
+          }
           letterStyle.backgroundColor = "#C0C0C0";
+        } else if (numOfLetSecretWord) {
+          letterStyle.backgroundColor = "#ffff00";
         }
-        chopSecretWord.splice(chopIndex, 1);
         idx++;
       }
-      if (obj.currWord === obj.secretWord) console.log("You winner!!");
-      obj.currWord = "";
-      obj.currIdx = obj.index;
+      if (currWord === secretWord) console.log("You winner!!");
+      this.currWord = "";
+      this.currIdx = index;
     }
   },
 
@@ -94,12 +113,21 @@ const obj = {
     }
     return false;
   },
+
+  numOfLetter(word, letter) {
+    let count = 0;
+    for (const ell of word) {
+      if (ell === letter) count++;
+    }
+    return count;
+  },
+
   restart() {
     obj.getSekretWord();
-    currWord = "";
-    index = 0;
-    currIdx = 0;
-    valid = false;
+    obj.currWord = "";
+    obj.index = 0;
+    obj.ocurrIdx = 0;
+    obj.valid = false;
     for (const letter of letters) {
       letter.innerText = "";
       letter.style.backgroundColor = "";
